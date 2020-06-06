ab2c33f61bcea9ee48276dfff9a07e892bf4922d
Karolina Och-Pudło <okaa93@gmail.com>
2020-06-06 10:53:35 +0200

    add elements dimensions in js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
-const listEl = document.querySelector('.offers__list');
-const itemEls = document.querySelectorAll('.offers__item');
-const listDot = document.querySelector('.offers__dots');
-const itemDots = document.querySelectorAll('.offers__dot');
+const offersList = document.querySelector('.offers__list');
+const offersItems = document.querySelectorAll('.offers__item');
+const dotList = document.querySelector('.offers__dots');
+const offersContainer = document.querySelector('.offers__container');
+const offersItemWidth = offersItems[0].offsetWidth;
+const offersItemsLength = offersItems.length;
+
+offersContainer.style.width = offersItemWidth + 'px';
+offersList.style.width = offersItemsLength * offersItemWidth + 'px';
 
 class DotItem {
     constructor(val) {
@@ -25,7 +30,7 @@ class DotsList {
     constructor() {
         this.index = 0;
         this.itemsList = [];
-        this.loadDotsList(itemEls.length);
+        this.loadDotsList(offersItems.length);
         this.renderDotsList();
         this.clickPrev();
         this.clickNext();
@@ -33,29 +38,31 @@ class DotsList {
 
     loadDotsList(dotsLength) {
         let i = 0;
+
         while (i < dotsLength) {
             let newEl = new DotItem(i);
+
             this.itemsList.push(newEl);
             i++;
         }
     }
 
     renderDotsList() {
-
-        listDot.innerHTML = "";
         let stringItems = '';
+
+        dotList.innerHTML = "";
         this.itemsList.map(item => {
             stringItems += `<li onclick="getSlide(event)" class="offers__dot ${item.class}" data-index="${item.val}"></li>`
         })
-        listDot.innerHTML = stringItems;
+        dotList.innerHTML = stringItems;
     }
 
     lowerIndex() {
-        this.index = this.index === 0 ? itemEls.length - 1 : this.index -= 1;
+        this.index = this.index === 0 ? offersItems.length - 1 : this.index -= 1;
     }
 
     higherIndex() {
-        this.index = this.index === itemEls.length - 1 ? 0 : this.index += 1;
+        this.index = this.index === offersItems.length - 1 ? 0 : this.index += 1;
     }
 
 
@@ -63,7 +70,7 @@ class DotsList {
         prevBtn.addEventListener('click', () => {
             this.itemsList[this.index].clearItemClass();
             this.lowerIndex();
-            listEl.style.transform = "translateX(" + (-this.index * 100) + "px)";
+            this.moveSlide();
             this.itemsList[this.index].setItemClass();
             this.renderDotsList();
         })
@@ -73,30 +80,27 @@ class DotsList {
         nextBtn.addEventListener('click', () => {
             this.itemsList[this.index].clearItemClass();
             this.higherIndex();
-            listEl.style.transform = "translateX(" + (-this.index * 100) + "px)";
-            console.log(this.index);
+            this.moveSlide();
             this.itemsList[this.index].setItemClass();
             this.renderDotsList();
         })
     }
 
     moveSlide() {
-        listEl.style.transform = "translateX(" + (-this.index * 100) + "px)";
+        offersList.style.transform = "translateX(" + (-this.index * offersItemWidth) + "px)";
     }
  
     clickDot(ev) {
         const listElements = ev.target.closest('.offers__dots').querySelectorAll('.offers__dot');
-        console.log('hello');
+        
         listElements.forEach(el => {
             el.classList.remove('offers__dot--active');
         })
         ev.target.classList.add('offers__dot--active');
         this.index = Number(ev.target.dataset.index);
         this.moveSlide();
-        console.log(this.index);
     }
 }
-
 
 let newDotsList = new DotsList();
 
